a216dfbb93fcc4fc6fdcf44a4cc1ab9e41795d92
Ambyjkl <ambareeshbalaji@gmail.com>
2017-11-05 17:31:57 -0500

    Increase coverage

--- tests/stl/list_test.cpp
+++ tests/stl/list_test.cpp
@@ -29,16 +29,25 @@ TEST(list_tests, push_pop_remove_tests) {
     ASSERT_EQ(numlist.size(), 1);
     numlist.remove_at(0);
     ASSERT_EQ(numlist.size(), 0);
+    numlist.push_front(4);
+    ASSERT_EQ(numlist.size(), 1);
+    numlist.pop_back();
+    ASSERT_EQ(numlist.size(), 0);
+    numlist.push_back(5);
+    ASSERT_EQ(numlist.size(), 1);
+    numlist.pop_front();
+    ASSERT_EQ(numlist.size(), 0);
 }
 
 TEST(list_tests, indexing_tests) {
     List<uint16_t> numlist;
-    numlist.push_back(1);
+    numlist.push_front(1);
     numlist.push_back(2);
     numlist.push_front(3); // 3 1 2
     ASSERT_EQ(numlist.indexOf(3), 0);
     ASSERT_EQ(numlist.indexOf(1), 1);
     ASSERT_EQ(numlist.indexOf(2), 2);
+    ASSERT_EQ(numlist.indexOf(4), 3);
     ASSERT_EQ(numlist.at(0), 3);
     ASSERT_EQ(numlist.get(1), 1);
     ASSERT_EQ(numlist[2], 2);
