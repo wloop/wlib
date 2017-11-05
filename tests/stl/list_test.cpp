045988d054d9406fec6369e67a9f4431bfc8f750
Ambyjkl <ambareeshbalaji@gmail.com>
2017-11-05 16:09:48 -0500

    Move stuff to header file because of C++ issues

--- tests/stl/list_test.cpp
+++ tests/stl/list_test.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include "stl/List.h"
+#include <stl/List.h>
 #include <stdint.h>
 
 using namespace wlp;
@@ -10,34 +10,36 @@ TEST(list_tests, constructor_tests) {
 }
 
 TEST(list_tests, push_pop_remove_tests) {
-    List<uint8_t> numlist;
+    List<uint16_t> numlist;
     numlist.push_back(1);
     numlist.push_back(2);
     numlist.push_front(3);
     ASSERT_EQ(numlist.front(), 3);
     ASSERT_EQ(numlist.back(), 2);
-    ASSERT_EQ(numlist.length(), 3);
+    ASSERT_EQ(numlist.size(), 3);
     numlist.pop_back();
     numlist.pop_front();
-    ASSERT_EQ(numlist.front(), 2);
-    ASSERT_EQ(numlist.back(), 2);
+    ASSERT_EQ(numlist.front(), 1);
+    ASSERT_EQ(numlist.back(), 1);
     numlist.push_front(4);
     numlist.push_back(5);
     numlist.remove_at(1);
-    ASSERT_EQ(numlist.length(), 2);
+    ASSERT_EQ(numlist.size(), 2);
     numlist.remove_at(1);
-    ASSERT_EQ(numlist.length(), 1);
+    ASSERT_EQ(numlist.size(), 1);
     numlist.remove_at(0);
-    ASSERT_EQ(numlist.length(), 0);
+    ASSERT_EQ(numlist.size(), 0);
 }
 
 TEST(list_tests, indexing_tests) {
-    List<uint8_t> numlist;
+    List<uint16_t> numlist;
     numlist.push_back(1);
     numlist.push_back(2);
-    numlist.push_front(3);
-    ASSERT_EQ(numlist.indexOf(2), 1);
-    ASSERT_EQ(numlist.at(1), 2);
-    ASSERT_EQ(numlist.get(1), 2);
-    ASSERT_EQ(numlist[1], 2);
+    numlist.push_front(3); // 3 1 2
+    ASSERT_EQ(numlist.indexOf(3), 0);
+    ASSERT_EQ(numlist.indexOf(1), 1);
+    ASSERT_EQ(numlist.indexOf(2), 2);
+    ASSERT_EQ(numlist.at(0), 3);
+    ASSERT_EQ(numlist.get(1), 1);
+    ASSERT_EQ(numlist[2], 2);
 }
--- wlib/stl/List.h
+++ wlib/stl/List.h
@@ -23,7 +23,7 @@ class List {
     MEMORY_OVERLOAD
 private:
     struct m_Node {
-        T item;
+        T value;
         m_Node *pNext,*pPrev;
     };
 
@@ -32,28 +32,234 @@ private:
     m_Node* m_pEnd; // Pointers to start and end
 
 public:
-    List(); // Class constructor
-    ~List(); // Class destructor
+    /**
+     * Default Constructor creates an empty List
+     */
+    List():
+        m_len(0),
+        m_pStart(nullptr),
+        m_pEnd(nullptr) {}
 
-    void push_back(const T i); // Push item at the back of list
-    void push_front(const T i);// Push item at the front of the list
-    void pop_back(); // Pops item from back
-    void pop_front(); // Pops item from front
-    T front(); // get item from front
-    T back(); // get item from back
-    uint32_t size(); // Returns size of list
-    void clear(); // Clears list
-    void remove_at(uint32_t index); // Remove item at given index
-    T get(uint32_t index); // Get item at given index
-    T& at(uint32_t index); // Get item at given index
+    /**
+     * Default Destructor deallocates all the nodes
+     */
+    ~List() {
+        m_Node *pTmp;
+        while (m_pStart != nullptr) {
+            pTmp = m_pStart;
+            m_pStart = m_pStart->pNext;
+            memory_free(pTmp);
+        }
+    }
 
-    // Array operator
-    T& operator[](uint32_t index);
-    const T& operator[](uint32_t index) const;
+    /**
+     * Creates a new node and pushes it to the back of the list
+     *
+     * @param value the value to store at the new node
+     */
+    void push_back(const T value) {
+        m_Node *pNewNode = static_cast<m_Node*>(memory_alloc(sizeof(m_Node)));
+        pNewNode->value = value;
+        pNewNode->pNext = nullptr;
 
-    // Non - critical functions
-    uint32_t length();
-    uint32_t indexOf(const T& val); // given a item, find its index and return it, return length if not found
+        if (m_pStart == nullptr) {
+            m_pStart = pNewNode;
+            pNewNode->pPrev = nullptr;
+        } else {
+            m_pEnd->pNext = pNewNode;
+            pNewNode->pPrev = m_pEnd;
+        }
+        m_pEnd = pNewNode;
+        m_len++;
+    }
+
+    /**
+     * Creates a new node and pushes it to the front of the list
+     *
+     * @param value the value to store at the new node
+     */
+    void push_front(const T value) {
+        m_Node *pNewNode = static_cast<m_Node*>(memory_alloc(sizeof(m_Node)));
+        pNewNode->value = value;
+        pNewNode->pPrev = nullptr;
+
+        if (m_pEnd == nullptr) {
+            m_pEnd = pNewNode;
+            pNewNode->pNext = nullptr;
+        } else {
+            m_pStart->pPrev = pNewNode;
+            pNewNode->pNext = m_pStart;
+        }
+        m_pStart = pNewNode;
+        m_len++;
+    }
+
+    void pop_back() {
+        m_Node *pTmp = m_pEnd;
+        m_pEnd = m_pEnd->pPrev;
+        if (m_pEnd != nullptr) {
+            m_pEnd->pNext = nullptr;
+        } else {
+            m_pStart = nullptr;
+        }
+        memory_free(pTmp);
+        m_len--;
+    }
+
+    void pop_front() {
+        m_Node *pTmp = m_pStart;
+        m_pStart = m_pStart->pNext;
+        if (m_pStart != nullptr) {
+            m_pStart->pPrev = nullptr;
+        } else {
+            m_pEnd = nullptr;
+        }
+        memory_free(pTmp);
+        m_len--;
+    }
+
+    /**
+     * Returns the value at the start of the List
+     * DANGEROUS: will die if nullptr
+     *
+     * @return the value stored at the start of the list
+     */
+    T front() {
+        return m_pStart->value;
+    }
+
+    /**
+     * Returns the value at the end of the List
+     * DANGEROUS: will die if nullptr
+     *
+     * @return the value stored at the end of the list
+     */
+    T back() {
+        return m_pEnd->value;
+    }
+
+    /**
+     * Returns the length of the List
+     * DANGEROUS: will die if nullptr
+     *
+     * @return the length of the list
+     */
+    uint32_t size() {
+        return m_len;
+    }
+
+    void clear() {
+        m_Node *pTmp;
+        while (m_pStart != nullptr) {
+            pTmp = m_pStart;
+            m_pStart = m_pStart->pNext;
+            memory_free(pTmp);
+            m_len--;
+        }
+        m_pEnd = nullptr;
+    }
+
+    /**
+     * Removes the value stored at the given index
+     * DANGEROUS: will die if out of bounds
+     *
+     * @param index the index to remove at
+     */
+    void remove_at(uint32_t index) {
+        m_Node *pTmp = m_pStart;
+        while (index-- > 0) {
+            pTmp = pTmp->pNext;
+        }
+
+        if (pTmp->pPrev != nullptr) {
+            pTmp->pPrev->pNext = pTmp->pNext;
+        } else {
+            m_pStart = pTmp->pNext;
+        }
+
+        if (pTmp->pNext != nullptr) {
+            pTmp->pNext->pPrev = pTmp->pPrev;
+        } else {
+            m_pEnd = pTmp->pPrev;
+        }
+
+        memory_free(pTmp);
+        m_len--;
+    }
+
+    /**
+     * Return a copy of the value stored at the `index`
+     * DANGEROUS: will die if out of bounds
+     *
+     * @param index to get
+     * @return copy of the value at that index
+     */
+    T get(uint32_t index) {
+        m_Node *pTmp = m_pStart;
+        while (index-- > 0) {
+            pTmp = pTmp->pNext;
+        }
+        return pTmp->value;
+    }
+
+    /**
+     * Return a reference to the value stored at the `index`
+     * DANGEROUS: will die if out of bounds
+     *
+     * @param index to get
+     * @return reference to the value at that index
+     */
+    T& at(uint32_t index) {
+        m_Node *pTmp = m_pStart;
+        while (index-- > 0) {
+            pTmp = pTmp->pNext;
+        }
+        return pTmp->value;
+    }
+
+    /**
+     * Array indexing operator
+     * `at` behind the scenes
+     * DANGEROUS: will die if out of bounds
+     *
+     * @param index to get
+     * @return reference to the value at that index
+     */
+    T& operator[](uint32_t index) {
+        return at(index);
+    }
+
+    /**
+     * Array indexing operator const variant
+     * `at` behind the scenes
+     * DANGEROUS: will die if out of bounds
+     *
+     * @param index to get
+     * @return reference to the value at that index
+     */
+    const T& operator[](uint32_t index) const {
+        return at(index);
+    }
+
+    /**
+     * indexOf from Javascript land that finds the index of the
+     * first element from the beginning of the List that equals `value`
+     * but returns the length of the array instead of -1 on failure
+     * DANGEROUS: will die if out of bounds
+     *
+     * @param value value to search for
+     * @return reference to the value at that index
+     */
+    uint32_t indexOf(const T& value) {
+        m_Node *pTmp = m_pStart;
+        for (uint32_t i = 0; i < m_len; i++) {
+            if (pTmp->value == value) {
+                return i;
+            }
+            pTmp = pTmp->pNext;
+        }
+        return m_len;
+    }
 };
 
 } // namespace wlp
