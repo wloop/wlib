f8852dccb42a93d8647a67e9a517b7e9d32b2d0c
Heather D'Souza <heather.dsouza@hotmail.com>
2017-11-11 10:00:44 -0500

    fixed code styling and added doxygen documentation

--- lib/wlib/comm/PacketConstructor.cpp
+++ lib/wlib/comm/PacketConstructor.cpp
@@ -1,85 +1,87 @@
-//
-// Created by Heather D'Souza on 2017-11-07.
-//
+/** @file PacketConstructor.c
+ *  @brief Creates a packet for sending data from the slave systems to the main computer.
+ *
+ *  Implements the functions defined in PacketConstructor.h.
+ *
+ *  @author Heather D'Souza (heathkd)
+ *  @date November 7, 2017
+ *  @bug No known bugs.
+ */
 
 #include "../stl/Bitset.h"
-
 #include <iostream>
 #include "PacketConstructor.h"
 #include <math.h>
 #include <cmath>
 
-PacketConstructor::PacketConstructor()
-{
-    data[0] = 0;
-    data[1] = 0;
-    data[2] = 0;
+PacketConstructor::PacketConstructor() {
+    m_pdata[0] = 0;
+    m_pdata[1] = 0;
+    m_pdata[2] = 0;
 }
 
-PacketConstructor::~PacketConstructor()
-{
-    delete data;
+PacketConstructor::~PacketConstructor() {
+    delete m_pdata;
 }
 
-PacketConstructor::PacketConstructor(float* data)
-{
-    this->data = data;
-    SetStartAndEnd();
-    DataToBin();
+PacketConstructor::PacketConstructor(float* p_data) {
+    m_pdata = p_data;
+    setStartAndEnd();
+    dataToBin();
 }
 
-void PacketConstructor::SetStartAndEnd() {
-    array.set(0);
-    array.set(63);
+float* PacketConstructor::getData() {
+    return m_pdata;
 }
 
-void PacketConstructor::IntToBin(int c, int pos)
-{
-    //bit assignment
-    for(int i = 0; i < 10; i++)
-        ((1<<i) & c) != 0 ? array.set(pos+i):array.reset(pos+i);
+Bitset<64> PacketConstructor::getBitArray() {
+    return m_bitArray;
 }
 
-void PacketConstructor::FloatToBin(int c, int pos)
-{
-    //bit assignment
-    for(int i = 0; i < 7; i++) {
-        if (1 << i & c) {
-            array.set(pos + i);
-        }
-        else {
-            array.reset(pos + i);
-        }
-    }
+void PacketConstructor::setStartAndEnd() {
+    m_bitArray.set(0);
+    m_bitArray.set(63);
 }
 
-void PacketConstructor::DataToBin()
+void PacketConstructor::dataToBin()
 {
     int intPart, floatPart;
     int posArray [] = {10,20,28,38,46,56};
 
     for (int j=0;j<3;j++) {
         //negative or positive bit
-        if (data[j] < 0) {
-            array.set(posArray[j*2] - 1);
-            data[j] = data[j] * (-1);
+        if (m_pdata[j] < 0) {
+            m_bitArray.set(posArray[j*2] - 1);
+            m_pdata[j] = m_pdata[j] * (-1);
         }
         else {
-            array.reset(posArray[j*2]-1);
+            m_bitArray.reset(posArray[j*2]-1);
         }
 
         //separate integer and decimal part of float;
-        intPart = static_cast<int>(data[j]);
-        floatPart = abs(nearbyint((((data[j] - static_cast<float>(intPart)) * 100))));
+        intPart = static_cast<int>(m_pdata[j]);
+        floatPart = abs(nearbyint((((m_pdata[j] - static_cast<float>(intPart)) * 100))));
 
-        IntToBin(intPart, posArray[j*2]);
-        FloatToBin(floatPart, posArray[j*2+1]);
+        intToBin(intPart, posArray[j*2]);
+        floatToBin(floatPart, posArray[j*2+1]);
     }
 }
 
-Bitset<64> PacketConstructor::GetArray() {
-    return array;
+void PacketConstructor::intToBin(int num, int pos) {
+    //bit assignment
+    for(int i = 0; i < 10; i++)
+        ((1<<i) & num) != 0 ? m_bitArray.set(pos+i):m_bitArray.reset(pos+i);
 }
+
+void PacketConstructor::floatToBin(int num, int pos) {
+    //bit assignment
+    for(int i = 0; i < 7; i++)
+        ((1<<i) & num) != 0 ? m_bitArray.set(pos+i):m_bitArray.reset(pos+i);
+}
+
+
+
+
 
 
 
--- lib/wlib/comm/PacketConstructor.h
+++ lib/wlib/comm/PacketConstructor.h
@@ -1,6 +1,13 @@
-//
-// Created by Heather D'Souza on 2017-11-07.
-//
+/** @file PacketConstructor.h
+ *  @brief Creates a packet for sending data from the slave systems to the main computer.
+ *
+ *  PacketConstructor is an abstract class. Therefore, it cannot be instantiated.
+ *  Subclasses include SensorPacket, CommandPacket, StatePacket, and LogPacket.
+ *
+ *  @author Heather D'Souza (heathkd)
+ *  @date November 7, 2017
+ *  @bug No known bugs.
+ */
 
 #ifndef EMBEDDEDCPLUSPLUS_PACKETCONSTRUCTOR_H
 #define EMBEDDEDCPLUSPLUS_PACKETCONSTRUCTOR_H
@@ -9,65 +16,126 @@
 #include "../stl/Bitset.h"
 using namespace wlp;
 
-class PacketConstructor
-{
+/**
+ *  @brief Blueprint for a packet of type Bitset. Cannot be instatiated.
+*/
+class PacketConstructor {
 
-// CONSTRUCTOR/DESTRUCTOR
 public:
-    // Create a new Packet
-    PacketConstructor(); //need to have default constructor with no parameters
-    PacketConstructor(float *data);
-    // Destroy this packet freeing all dynamically allocated memory.
-    ~PacketConstructor();
+    PacketConstructor();
+    PacketConstructor(float *p_data); // Create a new Packet
+    ~PacketConstructor(); // Destroy this packet freeing all dynamically allocated memory.
 
-// ACCESSORS (methods that get information)
+    //Helper Methods
+    /** @brief Sets the start and end bits.
+     *
+     * Each packet should begin and end with a '1' bit so that
+     * parser can distinguish the end of one packet and the
+     * beginning of another.
+     *
+     * @return Void.
+    */
+    void setStartAndEnd();
+    /** @brief Sets the type and name of the packet.
+     *
+     * There are 4 types of packets: sensor, command, state, and log.
+     * This method sets the bit representation of these types and the name
+     * pertaining to the specific packet. This function must be defined in each
+     * subclass of PacketConstructor so that the child class may be instantiated.
+     *
+     * @return Void.
+    */
+    virtual void setTypeAndName()=0;
+    /** @brief Converts data points addressed by the float pointer to bits.
+     *
+     * Splits each data point into its integer and decimal parts. It send the integer
+     * part to intToBin() and the decimal part to floatToBin().
+     *
+     * @return Void.
+    */
+    void dataToBin();
+    /** @brief Converts the integer portion of a float to its 10 bit representation.
+     *
+     * @param num the integer portion of the float
+     * @param pos the position at which to set bits in the bit array
+     *
+     * @return Void.
+    */
+    void intToBin(int num, int pos);
+    /** @brief Converts the decimal portion of a float to its 7 bit representation.
+     *
+     * @param num the decimal portion of the float
+     * @param pos the position at which to set bits in the bit array
+     *
+     * @return Void.
+    */
+    void floatToBin(int num, int pos);
+    /** @brief Accessor method for the data pointer
+     *
+     * @return data pointer
+    */
+    float* getData();
+    /** @brief Accessor method for the bit array
+     *
+     * @return bit array
+    */
+    Bitset<64> getBitArray();
 
-
-// MUTATORS (methods that change things)
-
-// HELPER
-    void IntToBin(int c, int pos);
-    void FloatToBin(int c, int pos);
-    void DataToBin();
-    void SetStartAndEnd();
-    virtual void SetTypeAndName()=0;
-    Bitset<64> GetArray();
-
-// MEMBER VARIABLES
-    float *data = new float [3];
-    Bitset<64> array;
+private:
+    float *m_pdata = new float [3];
+    Bitset<64> m_bitArray;
 };
 
-class CommandPacket : public PacketConstructor {
-
+/**
+ *  @brief Child of PacketConstructor for sensor data.
+*/
+class SensorPacket : public PacketConstructor {
     using PacketConstructor::PacketConstructor;
-    void SetTypeAndName() override {
-        array.reset(1);
-        array.set(2);
+    void setTypeAndName() override {
+        getBitArray().reset(1);
+        getBitArray().reset(2);
     }
 };
 
+/**
+ *  @brief Child of PacketConstructor for command data.
+*/
+class CommandPacket : public PacketConstructor {
+    using PacketConstructor::PacketConstructor;
+    void setTypeAndName() override {
+        getBitArray().reset(1);
+        getBitArray().set(2);
+    }
+};
+
+/**
+ *  @brief Child of PacketConstructor for state data.
+ *
+ *  This data will be used to monitor the state of each part of the pod.
+*/
 class StatePacket : public PacketConstructor {
 
     public:
 
         StatePacket(float *data) : PacketConstructor(data) {
-            SetTypeAndName();
+            setTypeAndName();
         }
-    void SetTypeAndName() override {
-        array.set(1);
-        array.reset(2);
+    void setTypeAndName() override {
+        getBitArray().set(1);
+        getBitArray().reset(2);
     }
 };
 
+/**
+ *  @brief Child of PacketConstructor for log data.
+*/
 class LogPacket : public PacketConstructor {
 
     using PacketConstructor::PacketConstructor;
-    void SetTypeAndName() override {
-        array.set(1);
-        array.set(2);
+    void setTypeAndName() override {
+        getBitArray().set(1);
+        getBitArray().set(2);
     }
 };
 
-#endif //EMBEDDEDCPLUSPLUS_PACKETCONSTRUCTOR_H
 #endif //EMBEDDEDCPLUSPLUS_PACKETCONSTRUCTOR_H
