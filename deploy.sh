a8b3c13cd63518eceb5adf31a04aacfb783cbb77
Deep Dhillon <deep.dhill6@gmail.com>
2017-11-26 18:29:23 -0500

    Free order adjusted to match standard c++

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,16 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
+echo "Pushing to gh-pages"
+
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone $REPO out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
+
+echo "Removing current files"
 
 # Clean out existing contents
 rm -rf out/**/* || exit 0
@@ -42,6 +46,8 @@ if [ -z `git diff --exit-code` ]; then
     exit 0
 fi
 
+echo "Deploying to gh-pages"
+
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add .
@@ -60,3 +66,5 @@ rm deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
+
+echo "Deployed to gh-pages"
