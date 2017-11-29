2571f69d7ae24917c46bc6563b10ae51301b2ebb
Jeff Niu <jeffniu22@gmail.com>
2017-11-29 11:10:16 -0800

    Library files

---
+++ Types.h
@@ -0,0 +1,26 @@
+/**
+ * @file Types.h
+ * @brief Type definition of all the types we will be using
+ *
+ * @author Deep Dhillon
+ * @date November 12, 2017
+ * @bug No known bug
+ */
+
+#ifndef EMBEDDEDCPLUSPLUS_TYPES_H
+#define EMBEDDEDCPLUSPLUS_TYPES_H
+
+#include <stdint.h>
+
+namespace wlp {
+
+    // 16 bit sizes
+    typedef uint16_t size_type;
+    typedef int16_t diff_type;
+
+    // 32 bit sizes
+    typedef uint32_t size32_type;
+    typedef int32_t diff32_type;
+}
+
+#endif //EMBEDDEDCPLUSPLUS_TYPES_H
