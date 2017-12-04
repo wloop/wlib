a2b130b61eff031a1462f8824d03f3634f4e991d
IbrahimIrfan <1ibrahimirfan@gmail.com>
2017-12-04 16:36:20 -0500

    Refinements, documentation and array list switch

--- lib/wlib/stl/BinTree.h
+++ lib/wlib/stl/BinTree.h
@@ -1,122 +1,184 @@
+/** @file BinTree.h
+ *  @brief A binary tree implementation using a list.
+ *  
+ *  This file implements a standard
+ *  binary tree stored using the ArrayList class.
+ *  Includes functions to intialize, insert,
+ *  and modify tree
+ *
+ *  @author Ibrahim Irfan
+ *  @date December 3, 2017
+ *  @bug No known bugs.
+ */
+
 #ifndef WLIB_TREE_H
 #define WLIB_TREE_H
 
-#include <stdint.h>
-#include <stdlib.h>
 #include  "ArrayList.h"
-#include "../memory/Memory.h"
 
-// TODO: delete below lines after array list implementation
-//#include <vector>
-using namespace std;
+using namespace wlp;
 
 /* Implement a (Binary) Tree using array list
- * TOOO: implement with our array list class instead of stdvetor
   */
 class Tree {
     public:
-        // Constructor - add a root to the tree
+        /**
+         * Constructor - initialize the tree with a root value
+         *
+         * @code
+         * Tree myTree = Tree(rootValue);
+         * @endcode
+         *
+         * @param rootVal Value for the root of the tree (first element)
+         */
         Tree(float rootVal) {
             m_tree.push_back(rootVal);
-            m_numLeaves = 1; // number of valid (non-empty) nodes
+            m_numLeaves = 1; // number of valid (non-null) nodes
             m_kNullVal = (float)-1.1999; // value for empty nodes
-            m_kTreeOrder = 2; // children per parent (2 for binary)
         }
 
-        // Return tree root if exists
-        float GetRoot(void) { 
-            if ((uint64_t) m_tree.size() > 0) {
-              return m_tree[0];
-            } 
-            return m_kNullVal;
+        /**
+         * Returns the root value of the tree
+         *
+         * @return Value at the root of the tree (first element)
+         */
+        float getRoot() { 
+            return m_tree.at(0);
         }
 
-        // Return the value at index in the vector
-        float GetValFromIndex(size_t index) {
-            return m_tree[index];
+        /**
+         * Returns the value at an index of the tree
+         *
+         * @param index Index of element desired
+         * @return Value at index @p index
+         */
+        float getVal(size_t index) {
+            return m_tree.at(index);
         }
 
-        // Insert a child node given the index of the parent
-        // Return the child's index
-        int64_t InsertChildWithParent(size_t parentIndex, float childVal) {
-            int64_t childIndex = ((int64_t) m_kTreeOrder)*((int64_t)parentIndex) + 1;
+        /**
+         * Insert a child node given the index of the parent
+         *
+         * @param parentIndex Index of parent node
+         * @param childVal Value to insert
+         * @return Index of inserted element in the tree.
+         */
+        int64_t insertChildAt(size_t parentIndex, float childVal) {
+            int64_t childIndex = 2*((int64_t)parentIndex) + 1;
             int64_t len = (int64_t) m_tree.size();
+
+            ArrayList<int>::const_iterator cit = m_tree.begin();
 
             // insert null nodes to fill gap if needed
             while (childIndex > len) {
-                m_tree.insert(m_tree.begin() + len, m_kNullVal);
+                m_tree.insert(cit + len, m_kNullVal);
                 len++;
             }
 
             // insert the node and increase valid node count
-            m_tree.insert(m_tree.begin() + childIndex, childVal);
+            m_tree.insert(cit + childIndex, childVal);
             m_numLeaves++;
             return childIndex;
         }
          
-        // returns pointer to array of the two children
-        float* GetChildrenValsFromParent(size_t parentIndex){
-            float *p_children = (float *) malloc(2*sizeof(float));
-            bool child = 0;
-
-            if (m_tree[2*parentIndex + 1] != m_kNullVal) {
-                p_children[child] = m_tree[2*parentIndex + 1];
-                child = 1;
-            }
-
-            if (m_tree[2*parentIndex + 2] != m_kNullVal) {
-                p_children[child] = m_tree[2*parentIndex + 2];
-            }
-
-            return p_children;
+        /**
+         * Get the index of the right child of a parent node
+         * 
+         * @param parentIndex Index of parent node
+         * @return Index of right child of parent.
+         * @note The right child is the child that is inserted first. 
+         * If there is only one child, it is the left child.
+         */
+        float getRightChildIndex(size_t parentIndex){
+            return 2*parentIndex + 1;
         }
         
-        // returns pointer to array of the two children
-        size_t* GetChildrenIndexFromParent(size_t parentIndex){
-            size_t *p_children = (size_t *) malloc(2*sizeof(size_t));
-            bool child = 0;
-
-            if (m_tree[2*parentIndex + 1] != m_kNullVal) {
-                p_children[child] = 2*parentIndex + 1;
-                child = 1;
-            }
-
-            if (m_tree[2*parentIndex + 2] != m_kNullVal) {
-                p_children[child] = 2*parentIndex + 2;
-            }
-
-            return p_children;
+       /**
+         * Get the index of the left child of a parent node
+         * 
+         * @param parentIndex Index of parent node
+         * @return Index of right child of parent.
+         * @note The left child is the child that is inserted second.
+         * If there is only one child, it is the left child.
+         */
+        float getLeftChildIndex(size_t parentIndex){
+            return m_tree.at(2*parentIndex + 1);
         }
 
-        // returns parent index given child index
-        size_t GetParentIndexFromChild(size_t childIndex){
+        /**
+         * Get the value of the right child of a parent node
+         * 
+         * @param parentIndex Index of parent node
+         * @return Value of right child of parent.
+         * @note The right child is the child that is inserted first. 
+         * If there is only one child, it is the left child.
+         */
+        float getRightChildVal(size_t parentIndex){
+            return m_tree.at(2*parentIndex + 1);
+        }
+        
+       /**
+         * Get the value of the left child of a parent node
+         * 
+         * @param parentIndex index of parent node
+         * @return index of left child of parent.
+         * @note The left child is the child that is inserted second.
+         * If there is only one child, it is the left child.
+         */
+        float getLeftChildVal(size_t parentIndex){
+            return m_tree.at(2*parentIndex + 1);
+        }
+        
+        /** 
+         * Returns parent index given child index
+         *
+         * @param childIndex index of parent node
+         * @return Index of the parent with child at @p childIndex
+         */
+        size_t getParentIndex(size_t childIndex){
           return ((childIndex-1)/2);
         }
         
-        // returns parent index given child index
-        float GetParentValFromChild(size_t childIndex){
-          return m_tree[(1/2)*(childIndex-1)];
+        /** 
+         * Returns parent value given child index
+         *
+         * @param childIndex index of parent node
+         * @return Value of the parent with child at @p childIndex
+         */
+        float getParentVal(size_t childIndex){
+          return m_tree.at((1/2)*(childIndex-1));
         }
 
-        // Clear the tree
-        void Clear() {
+        /** 
+         * Clears the tree.
+         */
+        void clear() {
             m_tree.clear();
             m_numLeaves = 0;
         }
 
-        // Get the number of leaves in the tree
-        uint64_t GetNumLeaves() {
+        /** 
+         * Returns the number of leaves (valid, non-empty nodes)
+         * in the tree.
+         *
+         * @return Number of leaves (valid, non-empty nodes) in the tree.
+         */
+        uint64_t getNumLeaves() {
           return m_numLeaves;
         }
 
     private:
-        // vector to store the tree
-        vector<float> m_tree;
-        // number of valid (non-empty) nodes in the tree
+        /**
+         * List to store the tree
+         */
+        ArrayList<int> m_tree([], 0);
+        /**
+         * Number of valid (non-empty) nodes in the tree
+         */
         uint64_t m_numLeaves;
-        // tree order (2 for binary)
-        uint64_t m_kTreeOrder;
-        // TODO: find better null value
+        /**
+         * Null value for empty nodes
+         */
         float m_kNullVal;
 };
 
--- tests/stl/tree_check.cpp
+++ tests/stl/tree_check.cpp
@@ -1,36 +1,42 @@
 #include "stl/BinTree.h"
 #include "gtest/gtest.h"
 
-
 // test initialization
 TEST(tree_test, test_root_val){
     float rootVal = 5;
     Tree tree = Tree(rootVal);
-    ASSERT_EQ(tree.GetRoot(), rootVal);
+    ASSERT_EQ(tree.getRoot(), rootVal);
 }
 
 // test inserting and deleting
 TEST(tree_test, test_insert_and_delete){
     Tree tree = Tree(0);
 
-    tree.InsertChildWithParent(0, 1);
-    tree.InsertChildWithParent(0, 2);
-    ASSERT_EQ(tree.GetNumLeaves(), 3);
+    tree.insertChild(0, 1);
+    tree.insertChild(0, 2);
+    ASSERT_EQ(tree.getNumLeaves(), 3);
+    ASSERT_EQ(tree.getVal(1), 2);
+    ASSERT_EQ(tree.getVal(2), 1);
 
-    float *childrenVals = tree.GetChildrenValsFromParent(0);
-    ASSERT_EQ(childrenVals[0], 2);
-    ASSERT_EQ(childrenVals[1], 1);
+    float leftChild = tree.getLeftChildVal(0);
+    float rightChild = tree.getRightChildVal(0);
+    ASSERT_EQ(leftChild, 2);
+    ASSERT_EQ(rightChild, 1);
 
-    tree.InsertChildWithParent(1, 3);
-    childrenVals = tree.GetChildrenValsFromParent(1);
-    ASSERT_EQ(childrenVals[0], 3);
+    tree.insertChild(1, 3);
+    float leftChild = tree.getLeftChildVal(1);
+    float rightChild = tree.getRightChildVal(1);
+    ASSERT_EQ(leftChild, 3);
+    ASSERT_EQ(rightChild, -1.1999); // null
 
-    size_t *childrenIndex = tree.GetChildrenIndexFromParent(1);
-    size_t parentIndex = tree.GetParentIndexFromChild(childrenIndex[0]);
-    ASSERT_EQ(parentIndex, 1);
+    size_t leftChildIndex = tree.getLeftChildIndex(1);
+    size_t rightChildIndex = tree.getRightChildIndex(1);
+    size_t parentIndex1 = tree.getParentIndex(leftChildIndex);
+    size_t parentIndex2 = tree.getParentIndex(rightChildIndex);
+    ASSERT_EQ(parentIndex1, parentIndex2, 1);
 
-    ASSERT_EQ(tree.GetNumLeaves(), 4);
+    ASSERT_EQ(tree.getNumLeaves(), 4);
 
-    tree.Clear();
-    ASSERT_EQ(tree.GetNumLeaves(), 0);
+    tree.clear();
+    ASSERT_EQ(tree.getNumLeaves(), 0);
 }
