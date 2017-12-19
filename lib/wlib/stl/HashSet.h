f15dfede8306bb5269ea2aecc9222e9f00b297f6
Jeff Niu <jeffniu22@gmail.com>
2017-12-19 10:53:13 -0800

    Refitted hashset with hashtable

--- lib/wlib/stl/HashSet.h
+++ lib/wlib/stl/HashSet.h
@@ -13,221 +13,129 @@
 #ifndef EMBEDDEDCPLUSPLUS_CHAINSET_H
 #define EMBEDDEDCPLUSPLUS_CHAINSET_H
 
-#include "HashMap.h"
 #include "Equal.h"
 #include "Hash.h"
+#include "HashTable.h"
+#include "Table.h"
 
 namespace wlp {
 
     /**
      * Hash set implementation using separate chaining. This implementation
      * supports removal operations, unlike the open addressed set.
+     *
      * @tparam Key   the element type
      * @tparam Hash  the hash function
      * @tparam Equal the equality function
      */
     template<class Key,
-            class Hash = Hash<Key, uint16_t>,
-            class Equal = Equal<Key>>
+            class Hasher = Hash <Key, uint16_t>,
+            class Equals = Equal <Key>>
     class HashSet {
     public:
-        typedef HashSet<Key, Hash, Equal> set_type;
-        typedef HashMap<Key, Key, Hash, Equal> table_type;
-        typedef typename HashMap<Key, Key, Hash, Equal>::iterator iterator;
-        typedef typename HashMap<Key, Key, Hash, Equal>::const_iterator const_iterator;
+        typedef HashSet<Key, Hasher, Equals> set_type;
+        typedef HashTable<Key, Key, Key, SetGetKey<Key>, SetGetVal<Key>, Hasher, Equals> table_type;
+
+        typedef typename table_type::iterator iterator;
+        typedef typename table_type::const_iterator const_iterator;
         typedef typename table_type::size_type size_type;
         typedef typename table_type::percent_type percent_type;
-        typedef typename table_type::key_type key_type;
+
+        typedef Key key_type;
 
     private:
-        table_type m_hash_map;
+        table_type m_table;
 
     public:
         /**
          * Construct a chained hash set, which creates
          * the backing chain hash map instance.
+         *
          * @param n        the initial size of the backing array
          * @param max_load the maximum load factor before rehash
          */
-        explicit HashSet(
-                size_type n = 12,
-                percent_type max_load = 75)
-                : m_hash_map(n, max_load) {
+        explicit HashSet(size_type n = 12, percent_type max_load = 75)
+                : m_table(n, max_load) {
         }
 
-        /**
-         * Disable copy constructor.
-         */
         HashSet(const set_type &) = delete;
 
-        /**
-         * Move constructor.
-         *
-         * @param set hash set to move
-         */
         HashSet(set_type &&set)
-                : m_hash_map(move(set.m_hash_map)) {
+                : m_table(move(set.m_table)) {
         }
 
-        /**
-         * @return the current number of elements in the set
-         */
         size_type size() const {
-            return m_hash_map.size();
+            return m_table.size();
         }
 
-        /**
-         * @return the size of the backing array
-         */
         size_type capacity() const {
-            return m_hash_map.capacity();
+            return m_table.capacity();
         }
 
-        /**
-         * @return the maximum load factor
-         */
         percent_type max_load() const {
-            return m_hash_map.max_load();
+            return m_table.max_load();
         }
 
-        /**
-         * @return whether the hash map is empty
-         */
         bool empty() const {
-            return m_hash_map.empty();
+            return m_table.empty();
         }
 
-        /**
-         * @return a pointer to the backing hash map
-         */
         const table_type *get_backing_table() const {
-            return &m_hash_map;
+            return &m_table;
         }
 
-        /**
-         * An iterator instance to the beginning of the hash set.
-         * The iterator order of the set is not guaranteed to
-         * be in any particular order.
-         * @return iterator to the first element, or @code end @endcode
-         * if the set is empty
-         */
         iterator begin() {
-            return m_hash_map.begin();
+            return m_table.begin();
         }
 
-        /**
-         * A pass-the-end iterator instance. This iterator means
-         * that an iterator has read past the end of the set
-         * and has become invalid.
-         * @return a pass-the-end iterator
-         */
         iterator end() {
-            return m_hash_map.end();
+            return m_table.end();
         }
 
-        /**
-         * @return a constant iterator to the first element
-         */
         const_iterator begin() const {
-            return m_hash_map.begin();
+            return m_table.begin();
         }
 
-        /**
-         * @return a constant pass-the-end iterator
-         */
         const_iterator end() const {
-            return m_hash_map.end();
+            return m_table.end();
         }
 
-        /**
-         * Empty the elements in the set, such that its
-         * size is now zero.
-         */
         void clear() noexcept {
-            m_hash_map.clear();
+            m_table.clear();
         }
 
-        /**
-         * Insert an element into the set. This function
-         * returns a pair consistent of the iterator to the
-         * element that was inserted or to the element that
-         * prevented insertion. The second boolean value indicates
-         * whether insertion took place.
-         * @param key the element to insert
-         * @return a pair of an iterator and boolean
-         */
         template<typename K>
         Pair<iterator, bool> insert(K &&key) {
-            return m_hash_map.insert(forward<K>(key), forward<K>(key));
+            return m_table.insert_unique(key);
         };
 
-        /**
-         * Check if an element is in the set.
-         * @param key element whose existence to check
-         * @return true if the element is contained in the set
-         */
         bool contains(const key_type &key) const {
-            return m_hash_map.contains(key);
+            return m_table.find(key) != m_table.end();
         }
 
-        /**
-         * Obtain an iterator to an element in the set.
-         * @param key the element to find
-         * @return iterator to the element or pass-the-end
-         * if the element is not in the set
-         */
         iterator find(const key_type &key) {
-            return m_hash_map.find(key);
+            return m_table.find(key);
         }
 
-        /**
-         * Obtain a const iterator to an element in the set
-         * @param key the element to find
-         * @return const iterator to the element or pass-the-end
-         * if the element is not in the set
-         */
         const_iterator find(const key_type &key) const {
-            return m_hash_map.find(key);
+            return m_table.find(key);
         }
 
-        /**
-         * Erase an element pointed to be an iterator.
-         * This function will move the provided iterator
-         * to the next element and return a copy of
-         * that iterator.
-         * @param pos the iterator whose element to erase
-         * @return an iterator to the next element, or
-         * pass the end if the iteration has reached the end
-         */
         iterator erase(const iterator &pos) {
-            return m_hash_map.erase(pos);
+            iterator tmp = pos;
+            ++tmp;
+            m_table.erase(pos);
+            return tmp;
         }
 
-        /**
-         * Remove a value from the set.
-         * @param key the value to remove
-         * @return true if the value was removed,
-         * false if the value was never in the set
-         */
         bool erase(const key_type &key) {
-            return m_hash_map.erase(key);
+            return m_table.erase(key) > 0;
         }
 
-        /**
-         * Disable copy assignment.
-         *
-         * @return reference to this set
-         */
         set_type &operator=(const set_type &) = delete;
 
-        /**
-         * Move assignment operator.
-         *
-         * @param set hash set to move
-         * @return reference to this set
-         */
         set_type &operator=(set_type &&set) {
-            m_hash_map = move(set.m_hash_map);
+            m_table = move(set.m_table);
             return *this;
         }
     };
