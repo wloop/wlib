417215b71ff1c003881214f22ec8315918a4020d
Travis CI User <travis@example.org>
2018-01-11 13:43:47 +0000

    Deploying Wlib Library: eaed9d72b46753d025f436cca001bebb20e8b8b4

--- memory/New.cpp
+++ memory/New.cpp
@@ -1,4 +1,11 @@
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
+#if (defined(__AVR_ATmega1280__)  || \
+     defined(__AVR_ATmega2560__)  || \
+     defined(__AVR_ATmega1284__)  || \
+     defined(__AVR_ATmega1284P__) || \
+     defined(__AVR_ATmega644__)   || \
+     defined(__AVR_ATmega644A__)  || \
+     defined(__AVR_ATmega644P__)  || \
+     defined(__AVR_ATmega644PA__)) && !defined(COSA_H)
 
 #include "New.h"
 
@@ -6,7 +13,7 @@ void *operator new(decltype(sizeof(0)), void *ptr) noexcept {
     return ptr;
 }
 
-#elif defined(WLIB_DEBUG)
+#elif defined(WLIB_DEBUG) && !defined(COSA_H)
 #else
 
 #include <stdlib.h>
