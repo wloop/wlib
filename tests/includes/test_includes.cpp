808020a78ca9fc16285007474e25d2fbb276a79f
Mogball <jeffniu22@gmail.com>
2018-06-01 16:31:46 -0400

    namespaced includes

---
+++ tests/includes/test_includes.cpp
@@ -0,0 +1,40 @@
+#include <wlib/array_heap>
+#include <wlib/array_list>
+#include <wlib/array2d>
+#include <wlib/bit_set>
+#include <wlib/comparator>
+#include <wlib/dynamic_string>
+#include <wlib/equals>
+#include <wlib/exception>
+#include <wlib/hash>
+#include <wlib/hash_map>
+#include <wlib/hash_set>
+#include <wlib/hash_table>
+#include <wlib/initializer_list>
+#include <wlib/link_list>
+#include <wlib/math>
+#include <wlib/memory>
+#include <wlib/open_map>
+#include <wlib/open_set>
+#include <wlib/open_table>
+#include <wlib/pair>
+#include <wlib/shared_ptr>
+#include <wlib/state_machine>
+#include <wlib/static_string>
+#include <wlib/string>
+#include <wlib/tree>
+#include <wlib/tree_map>
+#include <wlib/tree_set>
+#include <wlib/tuple>
+#include <wlib/type_traits>
+#include <wlib/types>
+#include <wlib/unique_ptr>
+#include <wlib/utility>
+#include <wlib/vector2d>
+#include <wlib/wlib>
+
+void include_test() {
+    array_list<int> list;
+    list.push_back(1);
+}
+
