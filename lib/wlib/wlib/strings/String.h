9477815228dbee424518c82a5986084dce66a0f4
Jeff Niu <jeffniu22@gmail.com>
2018-06-05 14:21:17 -0400

    Added non-const c_str (#132)
    * Added non-const c_str
    
    * added string resize and replace
    
    * Removed realloc calls
    
    * f

--- lib/wlib/wlib/strings/String.h
+++ lib/wlib/wlib/strings/String.h
@@ -412,7 +412,16 @@ namespace wlp {
         }
 
         /**
-         * Provides access to character array that string uses behind the screen
+         * Provides access to the backing character array.
+         *
+         * @return character array
+         */
+        char *c_str() {
+            return m_buffer;
+        }
+
+        /**
+         * Provides access to the backing character array.
          *
          * @return character array
          */
@@ -996,11 +1005,46 @@ namespace wlp {
         void pop_back();
 
         /**
-         * Provides access to character array that string uses behind the screen.
+         * Provides access to the backing character array.
+         *
+         * @return character array
+         */
+        char *c_str();
+
+        /**
+         * Provides access to the backing character array.
          *
          * @return character array
          */
         const char *c_str() const;
+
+        /**
+         * If @code len > length() @endcode the string is resized
+         * to fit @code len @endcode characters, not including the
+         * null terminating. The new backing array size is
+         * @code len + 1 @endcode. The current content is copied.
+         *
+         * @param len the number of characters to fit
+         */
+        void resize(size_type len);
+
+        /**
+         * Discards the current string contents and replaces it
+         * with a string with @code len @endcode characters @code fill @endcode.
+         *
+         * @param len  the new string length
+         * @param fill the character with which to fill the string
+         */
+        void replace(size_type len, char fill);
+
+        /**
+         * Discards the current backing array and replaces it with
+         * one of size @code len + 1 @endcode. The contents are not
+         * initialized.
+         *
+         * @param len the number of characters to hold
+         */
+        void replace(size_type len);
 
         /**
          * Makes substring of the current string.
